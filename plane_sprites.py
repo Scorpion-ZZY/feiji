004ca0af2003189a8786066718ec2ea75c6eb554
beiyu <okzhangzongya@163.com>
2019-09-24 21:56:56 +0800

    四次提交-创建敌军飞机

--- plane_sprites.py
+++ plane_sprites.py
@@ -1,7 +1,13 @@
+import random
+
 import pygame
 
 # 指定屏幕大小常量
 SCREEN_RECT = pygame.Rect(0, 0, 480, 700)
+# 刷新的帧率
+FRAME_PER_SEC = 60
+# 创建敌机的定时器常量
+CREATE_ENEMY_EVENT = pygame.USEREVENT
 
 
 class GameSprite(pygame.sprite.Sprite):
@@ -20,6 +26,7 @@ class GameSprite(pygame.sprite.Sprite):
 
 class Background(GameSprite):
     """游戏背景精灵"""
+
     def __init__(self, is_alt=False):
         super().__init__("./images/background.png")
         if is_alt:
@@ -29,3 +36,19 @@ class Background(GameSprite):
         super().update()
         if self.rect.y >= SCREEN_RECT.height:
             self.rect.y = -self.rect.height
+
+
+class Enemy(GameSprite):
+    """敌机进精灵"""
+
+    def __init__(self):
+        super().__init__("./images/enemy1.png")
+        self.speed = random.randint(1, 5)
+        self.rect.y = -self.rect.height
+        self.rect.x = random.randint(1, SCREEN_RECT.width-self.rect.width)
+
+
+    def update(self, *args):
+        super().update()
+        if self.rect.y >= SCREEN_RECT.height:
+            print("飞出屏幕.需要从精灵组删除")
--- ya_main.py
+++ ya_main.py
@@ -39,20 +39,27 @@ class PlanGame:
     """飞机大战主游戏"""
 
     def __init__(self):
+        # 游戏初始化
         print("游戏初始化...")
+
         # 创建主屏幕对象
         self.screen = pygame.display.set_mode(SCREEN_RECT.size)
+        # 设置窗口标题
+        pygame.display.set_caption("飞机大战...")
         # 创建时钟对象 设置帧率
         self.clock = pygame.time.Clock()
         # 运行精灵及精灵组的创建
         self.__create_sprites()
+        # 设置定时器时间  -- 创建敌机  每秒的频率
+        pygame.time.set_timer(CREATE_ENEMY_EVENT, 1000)
 
     def start_game(self):
+        # 启动游戏
         print("游戏开始...")
 
         while True:
             # 设置刷新帧率
-            self.clock.tick(60)
+            self.clock.tick(FRAME_PER_SEC)
             #  事件监听
             self.__event_handler()
             #  碰撞检测
@@ -71,18 +78,29 @@ class PlanGame:
         # bg2.rect.y = -bg2.rect.height
         self.back_group = pygame.sprite.Group(bg1, bg2)
 
+        # 创建精灵组
+        self.enemy_group = pygame.sprite.Group()
+
     def __event_handler(self):
+        # 实践监听
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 PlanGame.__game_over()
-
+            elif event.type == CREATE_ENEMY_EVENT:
+                print("敌机出场.....")
+                # 创建敌机精灵
+                enemy = Enemy()
+                self.enemy_group.add(enemy)
     def __check_collide(self):
+        # 碰撞检测
         pass
 
     def __update_sprites(self):
+        # 更新绘制画面
         self.back_group.update()
         self.back_group.draw(self.screen)
-
+        self.enemy_group.update()
+        self.enemy_group.draw(self.screen) # 精灵组使用draw()方法需要制定屏幕对象
     @staticmethod
     def __game_over():
         print("游戏结束")
